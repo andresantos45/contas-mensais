de8a7b09f44abac234c0c8e9c9c885a4ef7f3974
Andre <andreroza37@gmail.com>
2026-01-22 09:05:24 -0300

    teste de renderização

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,23 +1,32 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "./index.css";
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter } from "react-router-dom";
+// import App from "./App";
+// import "./index.css";
 
-// ChartJS (ok ficar aqui)
-import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
-ChartJS.register(ArcElement, Tooltip, Legend);
+// // ChartJS (ok ficar aqui)
+// import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+// ChartJS.register(ArcElement, Tooltip, Legend);
 
-const rootElement = document.getElementById("root");
+// const rootElement = document.getElementById("root");
 
-if (!rootElement) {
-  throw new Error("Elemento #root não encontrado no HTML");
+// if (!rootElement) {
+//   throw new Error("Elemento #root não encontrado no HTML");
+// }
+
+// ReactDOM.createRoot(rootElement).render(
+//   <React.StrictMode>
+//     <BrowserRouter>
+//       <App />
+//     </BrowserRouter>
+//   </React.StrictMode>
+// );
+
+export default function App() {
+  return (
+    <div style={{ padding: 40, color: "#fff" }}>
+      <h1>App renderizou</h1>
+      <p>Se você está vendo isso, o problema é rota/autenticação</p>
+    </div>
+  );
 }
-
-ReactDOM.createRoot(rootElement).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);

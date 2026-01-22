13f61fb3fdcf1e31255d4e9eeef842344dfaeae3
Andre <andreroza37@gmail.com>
2026-01-21 21:37:58 -0300

    fix: tentativa 1000

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,10 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/",               // 🔴 ESSENCIAL NO RENDER
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      input: "/index.html"
-    }
-  }
 });

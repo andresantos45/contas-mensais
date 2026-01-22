d8d7f8bbe7543d72b784ac058aaf0324f2162005
Andre <andreroza37@gmail.com>
2026-01-22 09:46:56 -0300

    fix: teste de correção 2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Dashboard from "./pages/Dashboard";
 import Login from "./pages/Login";
 import UsuariosAdmin from "./pages/UsuariosAdmin";
-import jwtDecode from "jwt-decode";
+import { jwtDecode } from "jwt-decode";
 
 function PrivateRoute({ children }) {
   const token = localStorage.getItem("token");

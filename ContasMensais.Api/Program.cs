3131c1a13946dbcd70d8ef18d520fdbf110199dc
Andre <andreroza37@gmail.com>
2026-02-02 18:30:57 -0300

    fix: parse Render DATABASE_URL for Npgsql

--- ContasMensais.Api/Program.cs
+++ ContasMensais.Api/Program.cs
@@ -61,8 +61,26 @@ builder.Services.AddAuthorization();
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     var connectionString =
-        builder.Configuration.GetConnectionString("DefaultConnection")
-        ?? Environment.GetEnvironmentVariable("DATABASE_URL");
+    builder.Configuration.GetConnectionString("DefaultConnection");
+
+    if (string.IsNullOrEmpty(connectionString))
+    {
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        if (!string.IsNullOrEmpty(databaseUrl))
+        {
+            var uri = new Uri(databaseUrl);
+            var userInfo = uri.UserInfo.Split(':');
+
+            connectionString =
+                $"Host={uri.Host};" +
+                $"Port={uri.Port};" +
+                $"Database={uri.AbsolutePath.TrimStart('/')};" +
+                $"Username={userInfo[0]};" +
+                $"Password={userInfo[1]};" +
+                $"SSL Mode=Require;Trust Server Certificate=true";
+        }
+    }
 
     options.UseNpgsql(connectionString);
 });
